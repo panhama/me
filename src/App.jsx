1605838c1d88d3bafd02cc63f1592a4b446b83cc
Panha Ma <panhama@live.com>
2022-09-06 00:18:31 -0500

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ function App() {
     return (
     <Router>
         <Routes>
-    <Route exact path="/rezoluti0n/" element={<div>  <Navigation/> <Banner/> <Skills/> <PolaroidBase/> <Photography/>  <Carousels/> <Footer/>  </div>}/>
+    <Route exact path="/" element={<div>  <Navigation/> <Banner/> <Skills/> <PolaroidBase/> <Photography/>  <Carousels/> <Footer/>  </div>}/>
   
-    <Route exact path="/rezoluti0n/photography" element={<div>  <Photography/> <Carousels/> <Footer/>  </div>}/>
+    <Route exact path="/photography" element={<div>  <Photography/> <Carousels/> <Footer/>  </div>}/>
 
-    <Route exact path="/rezoluti0n/portfolio" element={<Portfolio/>}/>
+    <Route exact path="/portfolio" element={<Portfolio/>}/>
     </Routes>
     </Router>
     );
