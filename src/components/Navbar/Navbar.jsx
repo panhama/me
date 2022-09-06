ab5c42b7ab06ee2cb5ad83164337e7052e5ef2c6
Panha Ma <panhama@live.com>
2022-09-06 00:54:08 -0500

    new alpha

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -47,7 +47,7 @@ export const Navigation = ()=>{
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Skills</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Projects</Nav.Link>
-              <Nav.Link href="/photography" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Photography</Nav.Link>
+              <Nav.Link href="/rezoluti0n/photography" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Photography</Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
