9976a2e838a4a21ba8f39141b7e83856d971669c
Panha Ma <panhama@live.com>
2022-09-08 16:08:27 -0500

    gh

--- src/components/Body/Error.jsx
+++ src/components/Body/Error.jsx
@@ -3,7 +3,7 @@ import Button2 from "../Buttons/Button 2";
 import "./Skills.css";
 export default function Error() {    
     return <div className="skills" 
-    style= {{padding:"22.55rem",
+    style={{padding:"23rem",height:"100vh",
     textAlign:"center"}}>
     <h1>COMING SOON</h1>
     <Button2 type="button" onClick={()=>{
