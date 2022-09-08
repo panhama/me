7ab875e3eb75f3fecd44e6cdd90203ce7b317ecd
Panha Ma <panhama@live.com>
2022-09-08 14:38:53 -0500

    y

--- src/components/Body/Error.jsx
+++ src/components/Body/Error.jsx
@@ -2,10 +2,10 @@ import { logDOM } from "@testing-library/react";
 import Button2 from "../Buttons/Button 2";
 import "./Skills.css";
 export default function Error() {    
-    return <div className="skills" style=
-    {{padding:"25.5rem",
+    return <div className="skills" 
+    style= {{padding:"22.55rem",
     textAlign:"center"}}>
-    <h2>COMING SOON</h2>
+    <h1>COMING SOON</h1>
     <Button2 type="button" onClick={()=>{
     window.location.href="#/"
     }} text="GO BACK">
